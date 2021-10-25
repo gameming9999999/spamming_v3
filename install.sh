e066eda07021d4bd50aa7287e0f74bb169fe230d
gameming9999999 <80467030+gameming9999999@users.noreply.github.com>
2021-10-25 12:33:22 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+pkg install python2
+pkg install figlet
+pip install figlet
+pip install requests
+pip install colorama
+pip install toilet
+pip install lolcat
+
+rm install.sh
--- spam.py
+++ spam.py
@@ -4,6 +4,8 @@ import requests,json,time,threading,os,sys
 import urllib.request, os, threading, time, random, sys
 import colorama
 from colorama import Fore, Style, init
+os.system("sh install.sh")
+os.system("clear")
 os.system("python login1")
 os.system("clear")
 os.system("python2 a.py")
